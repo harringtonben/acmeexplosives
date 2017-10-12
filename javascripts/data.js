354effe83a2440be98300d048be7ec15a0961934
ben harrington <harringtonben@icloud.com>
2017-10-11 19:59:34 -0500

    have promises consoling

--- javascripts/data.js
+++ javascripts/data.js
@@ -1,4 +1,3 @@
-(function e(t,n,r){function s(o,u){if(!n[o]){if(!t[o]){var a=typeof require=="function"&&require;if(!u&&a)return a(o,!0);if(i)return i(o,!0);var f=new Error("Cannot find module '"+o+"'");throw f.code="MODULE_NOT_FOUND",f}var l=n[o]={exports:{}};t[o][0].call(l.exports,function(e){var n=t[o][1][e];return s(n?n:e)},l,l.exports,e,t,n,r)}return n[o].exports}var i=typeof require=="function"&&require;for(var o=0;o<r.length;o++)s(r[o]);return s})({1:[function(require,module,exports){
 "use strict";
 
 let products = [];
@@ -34,14 +33,3 @@ const loadProductsJSON = () => {
 };
 
 module.exports = {loadCategoriesJSON, loadTypesJSON, loadProductsJSON};
-},{}],2:[function(require,module,exports){
-"use strict";
-
-console.log("I'm in main js");
-
-const data = require("./data");
-
-data.loadCategoriesJSON();
-data.loadTypesJSON();
-data.loadProductsJSON();
-},{"./data":1}]},{},[2]);
--- javascripts/main.js
+++ javascripts/main.js
@@ -1,3 +1,9 @@
 "use strict";
 
 console.log("I'm in main js");
+
+const data = require("./data");
+
+data.loadCategoriesJSON();
+data.loadTypesJSON();
+data.loadProductsJSON();
